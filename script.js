fd05bcf17db3659a58c52e56963b440dc2cf96b8
prdb <prdb@spb-rtk.ru>
2023-10-05 13:14:06 +0300

    made sidebar

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function startAnim() {
-    console.log('dsaf');
-    const makeShorter = {height: "110%"}
-    const icon = document.getElementById('sidebarIcon')
-    icon.animate(makeShorter, 1000)
+    const fullSidebar = document.getElementById('sidebarTerritory');
+    const sidebar = document.getElementById('sidebar');
+    const button = document.getElementById('sidebarIcon');
+    const pathToMove = -1 * sidebar.clientWidth - 10;
+    console.log(pathToMove);
+    if(button.style.rotate == "270deg") {
+        button.style.rotate = "90deg"
+    } else {
+        button.style.rotate = "270deg"
+    }
+    if (fullSidebar.style.left == pathToMove + "px") {
+        fullSidebar.style.left = "0"
+        console.log('did');
+    } else if(fullSidebar.style.left = "0") {
+        fullSidebar.style.left = pathToMove + "px";
+        console.log('ddd');
+    }
 }
