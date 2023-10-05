b82f4e7d35e4dffbe316e7dbed240455b310a04d
sharpaccount <vladikinastadiki@gmail.com>
2023-10-06 01:20:27 +0300

    fully changed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function startAnim() {
     const fullSidebar = document.getElementById('sidebarTerritory');
     const sidebar = document.getElementById('sidebar');
     const button = document.getElementById('sidebarIcon');
-    const pathToMove = -1 * sidebar.clientWidth - 10;
+    const pathToMove = -1 * sidebar.clientWidth - 2;
     console.log(pathToMove);
     if(button.style.rotate == "270deg") {
         button.style.rotate = "90deg"
